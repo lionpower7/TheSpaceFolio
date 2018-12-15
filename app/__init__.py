d228772a6abd1ed36591a3c1ebabfb21397dd348
Jeremy B. Frias <lionfocus07@gmail.com>
2018-12-15 15:55:54 -0500

    trying T Solve A Problem

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, request
+import os
+from os.path import basename
 
-app = Flask(__name__)
+#basedir gets the current working directory (where app.py is)
+basedir = os.path.abspath(os.path.dirname(file))
+#parentdir gets the previous directory of the basedir
+parentdir = os.path.abspath(os.path.join(basedir, os.pardir))
+
+app = Flask(name, template_folder=parentdir+"/templates/",
+static_folder=parentdir+"static",
+static_url_path=parentdir+"static")
 
 from app import routes
