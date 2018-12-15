ae4b88ec62f0c8764d6589231d64ce9fe55803b9
Jeremy B. Frias <lionfocus07@gmail.com>
2018-12-15 17:18:24 -0500

    Initial Setup Completed

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request
 import os
 from os.path import basename
 
-#basedir gets the current working directory (where app.py is)
-basedir = os.path.abspath(os.path.dirname(file))
-#parentdir gets the previous directory of the basedir
-parentdir = os.path.abspath(os.path.join(basedir, os.pardir))
 
-app = Flask(name, template_folder=parentdir+"/templates/",
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parentdir = dir_path = os.path.dirname(os.path.realpath(dir_path))
+
+print('PARENTDIR: {}'.format(parentdir))
+
+app = Flask('asdd', template_folder=parentdir+"/templates",
 static_folder=parentdir+"static",
 static_url_path=parentdir+"static")
 
