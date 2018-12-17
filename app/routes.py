271acf2aa045433eaf1dd6c63b81c9ee395ef7c2
Jeremy B. Frias <lionfocus07@gmail.com>
2018-12-17 15:03:49 -0500

    Problem Linking The Css File

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("Space.html")
+    return render_template("Space.html", Hello="Hello World")
